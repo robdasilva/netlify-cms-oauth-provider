6ee42e00d69b754a97dc87f744788a5a8f5a75a4
Robert Schweizer <mail@rschweizer.me>
2021-01-04 13:04:27 +0100

    Init repository
    Setup ESLint, Jest, Git Hooks (using `husky` and `lint-staged`), Prettier and TypeScript

---
+++ .eslintrc.js
@@ -0,0 +1,51 @@
+module.exports = {
+  env: {
+    es6: true,
+    jest: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier/@typescript-eslint',
+  ],
+  overrides: [
+    {
+      files: ['**/*.js'],
+      rules: {
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/no-var-requires': 'off',
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        custom: {
+          match: true,
+          regex: '^I[A-Z]',
+        },
+        format: ['PascalCase'],
+        selector: 'interface',
+      },
+    ],
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    camelcase: 'error',
+    curly: 'error',
+    eqeqeq: 'error',
+    'func-style': ['error', 'declaration', { allowArrowFunctions: true }],
+    'new-cap': 'error',
+    'newline-before-return': 'error',
+    'no-shadow': 'error',
+    'prefer-arrow-callback': 'error',
+    'prefer-const': 'error',
+    'sort-keys': 'error',
+    'spaced-comment': 'error',
+  },
+}
