cbe0c21130c7eb8505f34457eec77e7fe2e1e118
Robert Schweizer <mail@rschweizer.me>
2021-01-07 12:09:29 +0100

    Add basic handler unit tests

---
+++ src/auth/get-auth-callback.test.ts
@@ -0,0 +1,142 @@
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+  Callback,
+  Context,
+} from 'aws-lambda'
+import { warn } from 'lesslog'
+import { getOAuthClient, Provider } from '../utils/get-oauth-client'
+import { getScript } from '../utils/get-script'
+import { handler } from './get-auth-callback'
+
+jest.mock('lesslog')
+jest.mock('../utils/get-oauth-client')
+jest.mock('../utils/get-script')
+
+const context = { awsRequestId: 'jest' } as Context
+const proxyEvent = {} as APIGatewayProxyEvent
+
+describe('handler', () => {
+  afterEach(() => {
+    ;(getOAuthClient as jest.Mock).mockReset()
+  })
+
+  it('returns HTTP 200 response with auth script snippet', async (done) => {
+    const token = 'unit-test'
+    const getToken = jest
+      .fn()
+      // eslint-disable-next-line camelcase
+      .mockResolvedValueOnce({ token: { access_token: token } })
+    ;(getOAuthClient as jest.Mock).mockResolvedValueOnce({ getToken })
+
+    const script = 'success'
+    ;(getScript as jest.Mock).mockReturnValueOnce(script)
+
+    const provider = Provider.GITHUB
+    const code = '1234567890'
+    const domainName = 'auth.example.com'
+    const path = '/auth'
+    const event = {
+      ...proxyEvent,
+      pathParameters: { provider },
+      queryStringParameters: { code },
+      requestContext: {
+        ...proxyEvent.requestContext,
+        domainName,
+        path,
+      },
+    }
+
+    const callback: Callback<APIGatewayProxyResult> = (e, result) => {
+      expect(e).toBeNull()
+
+      expect(result).toStrictEqual(
+        expect.objectContaining({
+          body: script,
+          headers: { 'Content-Type': 'text/html' },
+          statusCode: 200,
+        })
+      )
+
+      expect(getOAuthClient).toHaveBeenCalledTimes(1)
+      expect(getOAuthClient).toHaveBeenCalledWith(provider)
+
+      expect(getToken).toHaveBeenCalledTimes(1)
+      expect(getToken).toHaveBeenCalledWith({
+        code,
+        // eslint-disable-next-line camelcase
+        redirect_uri: `https://${domainName}${path}`,
+      })
+
+      done()
+    }
+
+    expect(handler(event, context, callback)).toBeUndefined()
+  })
+
+  it('returns HTTP 200 response with error script snippet on error getting token', async (done) => {
+    const error = Object.assign(new Error('‾\\_(ツ)_/‾'), {
+      details: {
+        cause: 'Invalid code',
+      },
+    })
+    const getToken = jest.fn().mockRejectedValueOnce(error)
+    ;(getOAuthClient as jest.Mock).mockResolvedValueOnce({ getToken })
+
+    const script = 'error'
+    ;(getScript as jest.Mock).mockReturnValueOnce(script)
+
+    const provider = Provider.GITHUB
+    const code = '1234567890'
+    const domainName = 'auth.example.com'
+    const path = '/auth'
+    const event = {
+      ...proxyEvent,
+      pathParameters: { provider },
+      queryStringParameters: { code },
+      requestContext: {
+        ...proxyEvent.requestContext,
+        domainName,
+        path,
+      },
+    }
+
+    const callback: Callback<APIGatewayProxyResult> = (e, result) => {
+      expect(e).toBeNull()
+
+      expect(result).toStrictEqual(
+        expect.objectContaining({
+          body: script,
+          headers: { 'Content-Type': 'text/html' },
+          statusCode: 200,
+        })
+      )
+
+      expect(getOAuthClient).toHaveBeenCalledTimes(1)
+      expect(getOAuthClient).toHaveBeenCalledWith(provider)
+
+      expect(getToken).toHaveBeenCalledTimes(1)
+      expect(getToken).toHaveBeenCalledWith({
+        code,
+        // eslint-disable-next-line camelcase
+        redirect_uri: `https://${domainName}${path}`,
+      })
+
+      expect(warn).toHaveBeenCalledTimes(1)
+      expect(warn).toHaveBeenCalledWith(
+        'Could not get token from OAuth provider',
+        {
+          error: {
+            details: error.details,
+            message: error.message,
+            stack: error.stack,
+          },
+        }
+      )
+
+      done()
+    }
+
+    expect(handler(event, context, callback)).toBeUndefined()
+  })
+})
---
+++ src/auth/get-auth.test.ts
@@ -0,0 +1,51 @@
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+  Callback,
+  Context,
+} from 'aws-lambda'
+import { getOAuthClient, Provider } from '../utils/get-oauth-client'
+import { handler } from './get-auth'
+
+jest.mock('../utils/get-oauth-client')
+
+const context = { awsRequestId: 'jest' } as Context
+const proxyEvent = {} as APIGatewayProxyEvent
+
+describe('handler', () => {
+  it('returns HTTP 302 redirect response to authorization URL', async (done) => {
+    const url = 'http://auth.example.com/auth/callback'
+    const authorizeURL = jest.fn().mockReturnValueOnce(url)
+    ;(getOAuthClient as jest.Mock).mockResolvedValueOnce({ authorizeURL })
+
+    const provider = Provider.GITHUB
+    const event = {
+      ...proxyEvent,
+      queryStringParameters: { provider, scope: 'unit-test' },
+      requestContext: {
+        ...proxyEvent.requestContext,
+        domainName: 'auth.example.com',
+        path: '/auth',
+      },
+    }
+
+    const callback: Callback<APIGatewayProxyResult> = (e, result) => {
+      expect(e).toBeNull()
+
+      expect(result).toStrictEqual(
+        expect.objectContaining({
+          body: '',
+          headers: { Location: url },
+          statusCode: 302,
+        })
+      )
+
+      expect(getOAuthClient).toHaveBeenCalledTimes(1)
+      expect(getOAuthClient).toHaveBeenCalledWith(provider)
+
+      done()
+    }
+
+    expect(handler(event, context, callback)).toBeUndefined()
+  })
+})
